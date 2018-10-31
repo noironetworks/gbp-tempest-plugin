2bbd2035fdae504cc94f2fa61ba8358607b2e12c
Uemit Seren (Work) <uemit.seren@gmail.com>
2018-10-31 17:19:29 +0100

    Fixed NATPoolTest

--- gbp_tempest_plugin/tests/api/v2/test_nat_pool.py
+++ gbp_tempest_plugin/tests/api/v2/test_nat_pool.py
@@ -3,11 +3,11 @@ from tempest.lib import decorators
 from tempest.lib import exceptions as lib_exc
 from tempest.lib.common.utils import data_utils
 
-from gbp_tempest_plugin.tests import base
+from gbp_tempest_plugin.tests.api.v2.test_external_segment import ExternalSegmentTest
 
 LOG = logging.getLogger(__name__)
 
-class NatPoolTest(base.BaseGbpV2Test):
+class NATPoolTest(ExternalSegmentTest):
 
     @classmethod
     def setup_credentials(cls):
@@ -20,39 +20,54 @@ class NatPoolTest(base.BaseGbpV2Test):
         # creation of other neutron resources. NOTE: it must go before the
         # super call
         cls.set_network_resources()
-        super(NatPoolTest, cls).setup_credentials()
+        super(NATPoolTest, cls).setup_credentials()
 
     @classmethod
     def setup_clients(cls):
-        super(NatPoolTest, cls).setup_clients()
-        cls.client = cls.os_primary.nat_pool_client
+        super(NATPoolTest, cls).setup_clients()
+        cls.nat_client = cls.os_primary.nat_pool_client
+
+    @classmethod
+    def resource_setup(cls):
+        """This section is used to create any resources or objects which are
+           going to be used and shared by **all** test methods in the
+           TestCase. Note then anything created in this section must also be
+           destroyed in the corresponding resource_cleanup() method (which will
+           be run during tearDownClass())
+        """
+        super(NATPoolTest, cls).resource_setup()
+
+    def _create_nat_pool(self, name):
+        LOG.info('Create an External Segment')
+        segment = self._create_external_segment(data_utils.rand_name(self.__class__.__name__))
+        LOG.info('Create a NAT pool')
+        nat_pool = self.nat_client.create_nat_pool(name, segment['id'])['nat_pool']
+        self.addCleanup(self.nat_client.delete_nat_pool,nat_pool['id'])
+        return nat_pool
 
     def test_create_nat_pool(self):
-        LOG.info('Create a NAT pool')
-        body = self.client.create_nat_pool(name="test")
-        self.addCleanup(self.client.delete_nat_pool, body['nat_pool']['id'])
-        self.assertEqual("test", body['nat_pool']['name'])
+        name = data_utils.rand_name(self.__class__.__name__)
+        nat_pool = self._create_nat_pool(name)
+        self.assertEqual(name, nat_pool['name'])
 
     def test_list_nat_pools(self):
-        LOG.info('Create a NAT pool')
-        body = self.client.create_nat_pool(name="test")
-        self.addCleanup(self.client.delete_nat_pool, body['nat_pool']['id'])
+        name = data_utils.rand_name(self.__class__.__name__)
+        self._create_nat_pool(name)
         LOG.info('List NAT pools')
-        body = self.client.list_external_policies()
+        body = self.nat_client.test_list_nat_pools()
         self.assertGreater(len(body['nat_pools']), 0)
 
     def test_show_nat_pool(self):
-        LOG.info('Create a NAT pool')
-        body = self.client.create_nat_pool(name="test")
-        self.addCleanup(self.client.delete_nat_pool, body['nat_pool']['id'])
+        name = data_utils.rand_name(self.__class__.__name__)
+        nat_pool = self._create_nat_pool(name)
         LOG.info('Fetch a NAT pool')
-        body = self.client.show_nat_pool(body['nat_pool']['id'])
-        self.assertEqual("test", body['nat_pool']['name'])
+        body = self.nat_client.show_nat_pool(nat_pool['id'])
+        self.assertEqual(name, nat_pool['name'])
 
     def test_update_nat_pool(self):
-        LOG.info('Create a NAT pool')
-        body = self.client.create_nat_pool(name="test")
-        self.addCleanup(self.client.delete_nat_pool, body['nat_pool']['id'])
+        name = data_utils.rand_name(self.__class__.__name__)
+        new_name = data_utils.rand_name(self.__class__.__name__)
+        nat_pool = self._create_nat_pool(name)
         LOG.info('Update a NAT pool')
-        body = self.client.update_nat_pool(body['nat_pool']['id'], name="test2")
-        self.assertEqual("test2", body['nat_pool']['name'])
+        body = self.client.update_nat_pool(nat_pool['id'], name=new_name)
+        self.assertEqual(new_name, body['nat_pool']['name'])
